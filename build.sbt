c041cedfd5575541f62c6626f512cdd0a15b1bf9
russell <russell@radioactive.sg>
2016-05-10 19:12:17 +0800

    initial commit

---
+++ build.sbt
@@ -0,0 +1,3 @@
+resolvers += "Sonatype Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots"
+
+libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.12.5" % "test"
---
+++ src/main/scala/RestApi.scala
@@ -0,0 +1,8 @@
+
+object RestApi {
+
+  def POST(group : String, item : Int) : String = ???
+  def PUT(group : String, id : String, item : Int) : String = ???
+  def GET(id : String) : Int = ???
+
+}
